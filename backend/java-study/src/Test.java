c9c15e7494c7b05e3e2ed929cbff678bffc02b70
kaede221 <kaedeshimizu@qq.com>
2025-09-20 11:52:50 +0800

    feat(java-study): 实现洗牌, 准备牌, 发牌的操作

--- backend/java-study/src/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("准备进行集合进阶部分的综合学习");
-    }
-}
---
+++ backend/java-study/src/com/lc/doudizhu/App.java
@@ -0,0 +1,14 @@
+package com.lc.doudizhu;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+        先在控制台实现基本的三个步骤: 准备牌, 洗牌, 发牌
+        ♠ ♣ ♦ ♥
+        3 4 5 6 7 8 9 10 J Q K A 2
+         */
+
+        // 不过 App就是用来启动程序的, 所以这里不写主要的逻辑, 主要写在 PokerGame里面
+        new PokerGame();
+    }
+}
