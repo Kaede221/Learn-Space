fea19e6a07ca5aeaaf2866bc4c1a0abfdcade427
Kaede221 <kaedeshimizu@qq.com>
2025-10-19 17:20:23 +0800

    feat: 配置新的命令

--- frontend/code-learn/src/extension.ts
+++ frontend/code-learn/src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand('code-learn.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World!!!');
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-learn" is now active!');
+  context.subscriptions.push(disposable);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-learn.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-learn!');
-	});
-
-	context.subscriptions.push(disposable);
+  // 可以注册新的命令
+  // NOTE 这里需要注意, 如果需要注册新的命令, 则需要在 package.json 中也配置命令, 否则无效
+  context.subscriptions.push(
+    vscode.commands.registerCommand('code-learn.helloWorldWarn', () => {
+      // 可以使用别的API, 比如展示警告信息
+      vscode.window.showWarningMessage('这是一条警告信息', {
+        detail: '这是详细信息',
+      });
+    }),
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
