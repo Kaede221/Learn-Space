f075c576436ff9dcdd366f142e16f9a3190a2c53
Kaede221 <kaedeshimizu@qq.com>
2025-10-31 15:10:41 +0800

    bun: 学习了如何启动项目

--- frontend/bun-learn/.history/05-第一个项目/bun-project/index.ts
+++ frontend/bun-learn/.history/05-第一个项目/bun-project/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+// 正常引入数据就好
+import chalk from "chalk";
+
+// 可以直接使用库的方法
+console.log(chalk.blue("Kaede") + " " + chalk.yellow("Greet"));
+
+// 另外, 可以直接通过 bun start 来运行这个文件, 不需要再bun src/index.ts 这样运行了
