85cf9dfe498e4d24c89fa0e9f42071afe4a96c63
Kaede221 <kaedeshimizu@qq.com>
2025-11-01 12:05:31 +0800

    reinit: 使用bun重新初始化nextjs项目

--- frontend/nextjs-learn/src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-// 可以这样直接实现一个后端接口
-export async function GET(req: Request) {
-  console.log("请求参数为", req);
-  return NextResponse.json({
-    message: "Hello World From API",
-  });
-}
