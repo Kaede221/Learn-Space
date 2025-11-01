43e3f262af8c2a031a842024c6596a7a4eb4bbfd
Kaede221 <kaedeshimizu@qq.com>
2025-11-01 10:11:19 +0800

    bun: 学习了查询参数和路由参数

--- frontend/bun-learn/.history/06-Elysia/bun-project/index.ts
+++ frontend/bun-learn/.history/06-Elysia/bun-project/index.ts
@@ -20,6 +20,41 @@ app.get("/api", () => {
   };
 });
 
+// 可以实现动态路由
+app.get("/users/:id", (content) => {
+  // 会自动解析, 放入 content.params
+  const userId = content.params.id;
+
+  return {
+    message: `你正在访问 ID = ${userId} 的用户信息.`,
+  };
+});
+
+// 也可以添加查询参数
+app.get("/search", (content) => {
+  // 查询参数会自动放入 query 中
+  // 这里的q就是说, 需要有一个q的查询参数
+  const { q } = content.query;
+  if (!q) {
+    return "请使用查询参数 q";
+  }
+  return {
+    message: "你的参数为: " + q,
+  };
+});
+
+// NOTE Exercise 综合练习
+app.get("/greet/:name", (content) => {
+  const { name } = content.params;
+  const { title } = content.query;
+  if (name && !title) {
+    return `你好, ${name}!`;
+  } else if (name && title) {
+    return `你好, ${title} ${name}!`;
+  }
+  return "至少需要使用name参数";
+});
+
 // 最后, 启动服务器即可
 app.listen(3000);
 console.log(`Elysia 服务器正在 http://localhost:3000 上运行...`);
